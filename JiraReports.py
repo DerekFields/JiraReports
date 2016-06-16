b87a79b80a95f890f0ae47bb7e2b881a742d03f2
Derek Fields <derek@fields.name>
2016-06-16 15:43:55 -0400

    Started work on the Lift report. Was able to get list of the linked issues. Need to determine the work effort associated with each linked issue.

--- JiraReports.py
+++ JiraReports.py
@@ -40,13 +40,17 @@ gUsers = (
     "Yeon-Jun Kim",
 )
 
+
 def command_line():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u','--user', required=True)
-    parser.add_argument('-p','--password', required=True)
+    parser.add_argument('-u', '--user', required=True)
+    parser.add_argument('-p', '--password', required=True)
+    parser.add_argument('-m', '--missing', action='store_true', help='Print the missing work log report')
+    parser.add_argument('-l', '--lift', action='store_true', help='Print the Lift work report')
     return parser.parse_args()
 
-def jira_missing_time_report(interval):
+
+def missing_time_report(interval):
     """
 checks for time reporters who have not logged hours in the prior week
 
@@ -62,8 +66,9 @@ checks for time reporters who have not logged hours in the prior week
     jira_db = jira.JIRA(server=gServer, basic_auth=(gU, gP))
 
     ''' get the list of issues that have a worklog during the prior week'''
-    issue_list = jira_db.search_issues('worklogDate >= startOfWeek(-{0}d) AND worklogDate < startOfWeek()'.format(interval),
-                                       maxResults=False)
+    issue_list = jira_db.search_issues(
+        'worklogDate >= startOfWeek(-{0}d) AND worklogDate < startOfWeek()'.format(interval),
+        maxResults=False)
 
     print("Date Range: {0}-{1}".format(wstart.strftime("%m/%d/%Y"), wend.strftime("%m/%d/%Y")))
 
@@ -80,26 +85,27 @@ checks for time reporters who have not logged hours in the prior week
                 else:
                     loggers[name] = wlog.timeSpentSeconds
 
-#    l = list(loggers.keys())
-#    l.sort()
-#    for name in l:
-#        print("{0}: {1}".format(name, loggers[name] / 3600))
+                    #    l = list(loggers.keys())
+                    #    l.sort()
+                    #    for name in l:
+                    #        print("{0}: {1}".format(name, loggers[name] / 3600))
 
     print("\n--Missing--\n")
     for n in [name for name in gUsers if name not in loggers.keys()]:
         print(n)
-    
-    print("""
-You have no time reported for the date range. Please enter your time as soon as possible. If you have been on vacation, please make a work log entry in CORPACT-8.
-    """)
+
+    print(
+        'You have no time reported for the date range. Please enter your time as soon as possible. '
+        'If you have been on vacation, please make a work log entry in CORPACT-8.'
+    )
 
 
 def jira_allocation_report():
-    '''
+    """
         Reports on all open tasks that have time remaining. This allows visualization across all projects of the
         work effort currently remaining
     :return: None
-    '''
+    """
 
     projects = dict()
 
@@ -120,10 +126,10 @@ def jira_allocation_report():
             if assignee in projects[project]['assignees']:
                 projects[project]['assignees'][assignee]['issues'].append(issue)
             else:
-                projects[project]['assignees'][assignee] = {'assignee':issue.fields.assignee, 'issues':[issue]}
+                projects[project]['assignees'][assignee] = {'assignee': issue.fields.assignee, 'issues': [issue]}
         else:
-            projects[project] = {'project':issue.fields.project}
-            projects[project]['assignees'] = {assignee:{'assignee':issue.fields.assignee,'issues':[issue]}}
+            projects[project] = {'project': issue.fields.project}
+            projects[project]['assignees'] = {assignee: {'assignee': issue.fields.assignee, 'issues': [issue]}}
 
     p_list = list(projects.keys())
     p_list.sort()
@@ -137,12 +143,56 @@ def jira_allocation_report():
                 minutes = 0
                 for issue in i_list:
                     minutes += issue.fields.timeestimate
-                print("\t{}\t{}\t{:,.2f}".format(assignee, len(i_list), minutes/3600))
+                print("\t{}\t{}\t{:,.2f}".format(assignee, len(i_list), minutes / 3600))
+
+
+def lift_report(interval):
+    """
+    Find the total allocation to Lift by looking at all of the worklogs for LIFT and any issues that are linked to a
+    LIFT issue
+
+    :param interval:
+    :return:
+    """
+    loggers = dict()
+
+    # find the date range that interests us for this report
+    today = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-4))).replace(hour=0, minute=0, second=0,
+                                                                                           microsecond=0)  # Fix this to EDT for now
+    wstart = today - datetime.timedelta(days=today.isoweekday() + interval)
+    wend = wstart + datetime.timedelta(days=interval)
+
+    jira_db = jira.JIRA(server=gServer, basic_auth=(gU, gP))
+
+    ''' get the list of issues that have a worklog during the prior week'''
+    issue_list = jira_db.search_issues('project=LIFT', maxResults=False)
+
+    print("Date Range: {0}-{1}".format(wstart.strftime("%m/%d/%Y"), wend.strftime("%m/%d/%Y")))
+
+    for issue in issue_list:
+        if len(issue.fields.issuelinks) > 0:
+            for link in issue.fields.issuelinks:
+                try:
+                    target = link.outwardIssue.key
+                    direction = 'outward'
+                except AttributeError:
+                    try:
+                        target = link.inwardIssue.key
+                        direction = 'inward'
+                    except AttributeError:
+                        target = "No link"
+                        direction = "No direction"
+                print("{0} {1} {2} to {3}".format(issue.key, link.type.name, direction, target))
+
 
 if __name__ == '__main__':
     args = command_line()
     gU = args.user
     gP = args.password
 
-    jira_missing_time_report(7)
-    # jira_allocation_report()
+    if args.missing:
+        missing_time_report(7)
+    if args.lift:
+        lift_report(7)
+
+        # jira_allocation_report()
